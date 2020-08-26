56c64acffcef77711c31a1ae75d1bab53e18f9b7
karatsuba <romanchuhan18@gmail.com>
2020-08-26 22:08:54 +0200

    log url

--- src/api/fetchRates.ts
+++ src/api/fetchRates.ts
@@ -13,6 +13,8 @@ const API_BASE = 'https://openexchangerates.org/api/latest.json?app_id=';
 const RATES_URL = `${API_BASE}${process.env.REACT_APP_EXCHANGE_APP_ID}`;
 
 export const fetchRates = async (): Promise<RatesResponse> => {
+    console.log(RATES_URL);
+
     // use real API only for production
     if (process.env.NODE_ENV === 'development') {
         return Promise.resolve(fakeRates);
