72fa7e4a6b1c23519975bf351c60a663c6688b22
karatsuba <romanchuhan18@gmail.com>
2020-08-27 23:36:57 +0200

    rates fetch improvements

--- src/api/fetchRates.ts
+++ src/api/fetchRates.ts
@@ -7,6 +7,7 @@ export interface RatesResponse {
     timestamp: number;
     base: string;
     rates: Rates;
+    error?: boolean;
 }
 
 const API_BASE = 'https://openexchangerates.org/api/latest.json?app_id=';
@@ -15,18 +16,17 @@ const RATES_URL = `${API_BASE}${process.env.REACT_APP_EXCHANGE_APP_ID}`;
 export const fetchRates = async (): Promise<RatesResponse> => {
     // use real API only for production due to limitation (1000 requests/month)
     if (process.env.NODE_ENV === 'development') {
-        return Promise.resolve(fakeRates);
+        return fakeRates;
     }
 
     try {
         const response = await fetch(RATES_URL);
+        const json: RatesResponse = await response.json();
 
-        if (!response.ok) {
+        if (json.error) {
             // if API calls overuse happened or other api related issues, return fake rates
-            return Promise.resolve(fakeRates);
+            return fakeRates;
         }
-
-        const json: Promise<RatesResponse> = response.json();
         return json;
     } catch (error) {
         throw error;
